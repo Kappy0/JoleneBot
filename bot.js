a46492f8cffb06d1f57bd8a1c52c26e400897c99
Ryan Klaproth <ryanklaproth@gmail.com>
2021-03-10 17:34:23 -0600

    Updating announced for multiple streamers
    Bot can now automatically check for streamers in my streams.json file and check/update the "announced" field for them. The logic for grabbing stream data is there, but haven't tested yet if that works, so that's next.

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const bot_settings = require("./botsettings.json");
 const discord = require("discord.js");
 const fetch = require("node-fetch"); //Used for Twitch API
+const fs = module.require("fs"); //fs is Node.js's native file system module
 
 bot = new discord.Client();
 
@@ -22,7 +23,66 @@ bot.on("ready", async() => {
 	for(var i = 0; i < stream_list.users.length; i++)
 	{
 		console.log(stream_list.users[i].username);
+		stream_list.users[i].announced = true;
+
+		fs.writeFile("streams.json", JSON.stringify(stream_list, null, 4), err => {
+							if(err) throw (err);
+						});
 	}
+
+	bot.setInterval(() => {
+		for(var i = 0; i < stream_list.users.length; i++)
+		{
+			let streamer = stream_list.users[i];
+
+			fetch(stream_URL + streamer.username, {
+				headers: api_headers,
+			}).then(response => response.json())
+			.then(body => {
+				let data = body.data;
+
+				if(data[0])
+				{
+					console.log("Got Twitch data!");
+					if(!streamer.announced)
+					{
+						streamer.announced = true;
+
+						let embed = new discord.MessageEmbed()
+							//.setAuthor(`${data[0].user_name} is now live!`)
+							.setDescription("https://twitch.tv/kappylp")
+							.addField("Title", data[0].title)
+							.addField("Game", data[0].game_name)
+							//.setThumbnail(data[0].thumbnail_url)
+							.setFooter("Started at " + data[0].started_at);
+
+						let notif_channel = bot.channels.cache.get('556936544682901512');
+						notif_channel.send(`@here ${data[0].user_name} is LIVE!`, {embed: embed});
+
+						fs.writeFile("streams.json", JSON.stringify(stream_list, null, 4), err => {
+							if(err) throw (err);
+						});
+					}
+					else console.log("Stream has already been announced!");
+				}
+				else
+				{
+					console.log("No one's streaming");
+					if(streamer.announced)
+					{
+						streamer.announced = false;
+
+						fs.writeFile("streams.json", JSON.stringify(stream_list, null, 4), err => {
+							if(err) throw (err);
+						});
+					}
+
+					console.log("Checking status...");
+					console.log(body);
+				}
+			}).catch((err) => console.log("Caught " + err.stack));
+		}
+	}, 60000);
 });
 
 bot.login(bot_settings.token);
